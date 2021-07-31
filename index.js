3e840bc55bcebb5bfb5ba8b9cb65c8ba6a764eab
T1MON <derbindima5@gmail.com>
2021-07-31 15:14:51 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const handlebars = require('express-handlebars')
+const todoRouter = require('./routes/todo')
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+const hbs = handlebars.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRouter)
+
+async function start() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://T1MON:330230@express-nodejs.jk9vv.mongodb.net/myFirstDatabase',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: false
+        })
+
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
